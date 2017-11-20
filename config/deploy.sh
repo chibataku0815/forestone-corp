ff5ac9e1ac757a4623cb7507a606581fbf7236c1
千葉工 <chibataku0815@gmail.com>
2017-11-20 13:22:26 +0900

    deploy

--- config/deploy.sh
+++ config/deploy.sh
@@ -4,7 +4,7 @@
 deploy_path=/home/kusanagi/html/DocumentRoot/wp-content/themes/
 
 # デプロイするプロジェクトの名前
-proj_name=forestone_corp
+proj_name=forestone-corp
 
 # リモートリポジトリのURL
 remote_path=git@github.com:chibataku0815/forestone-corp.git
@@ -37,32 +37,20 @@ confirm "Is it OK to deploy? [y/n]"
 # -AでSSH鍵をサーバーでも利用
 # StrictHostKeyCheckingで初SSH接続時の[yes/no]を無視
 ssh -A -o StrictHostKeyChecking=no ${host} "
-    # Gitがあるか確認
-    if ! type 'git' > /dev/null 2>&1; then
-        echo 'git not found...'
-        exit 1
-    fi
-
-    # デプロイ場所が無かったら作成
-    if [ ! -e ${deploy_path} ]; then
-        confirm 'Is it OK to create '${deploy_path}' dir?[y/n]'
-        mkdir -p ${deploy_path}
-    fi
 
     cd ${deploy_path}
 
     # プロジェクトのディレクトリがあるか確認
     # なければclone、あればpullをおこなう
     if [ ! -e ${proj_name} ]; then
-        git clone ${remote_path} ${proj_name}
         echo 'cloning success!'
     else
         cd ${proj_name}
         # pullもしくはfetch＆reset --hardを使う
         # 競合を起こさないよう、強制的に最新のリモートリポジトリに合わせる
         git fetch origin
-        git reset --hard origin/${branch}
-        # git pull origin ${branch}
+        git reset --hard origin/master
+        git pull origin ${branch}
         echo 'deployment success!'
     fi
 "
