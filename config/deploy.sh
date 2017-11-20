d29a85bf11cdb7dc53858dcd0639261fcbd69152
千葉工 <chibataku0815@gmail.com>
2017-11-20 18:27:34 +0900

    スターターテーマ作成

---
+++ config/deploy.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# デプロイしたいディレクトリの場所
+deploy_path=/home/kusanagi/html/DocumentRoot/wp-content/themes/
+
+# デプロイするプロジェクトの名前
+proj_name=forestone-corp
+
+# リモートリポジトリのURL
+remote_path=git@github.com:chibataku0815/forestone-corp.git
+
+# デプロイするブランチ
+branch=master
+
+# 本番環境へのSSHログイン情報（ユーザ名@ホスト名）
+host=root@133.130.101.97
+
+#############ここまで#############
+
+# yes/noで応答
+function confirm () {
+    echo -n $1
+    read answer
+    case `echo $answer | tr y Y` in
+        Y*)
+        ;;
+        *)
+            echo "Bye."
+            exit
+        ;;
+    esac
+}
+
+# 本当にデプロイするか確認
+# confirm "Is it OK to deploy? [y/n]"
+
+# -AでSSH鍵をサーバーでも利用
+# StrictHostKeyCheckingで初SSH接続時の[yes/no]を無視
+ssh -A -o StrictHostKeyChecking=no ${host} "
+
+    cd ${deploy_path}
+
+    # プロジェクトのディレクトリがあるか確認
+    # なければclone、あればpullをおこなう
+    if [ ! -e ${proj_name} ]; then
+        echo 'cloning success!'
+    else
+        cd ${proj_name}
+        echo 'cd OK'
+        git fetch origin
+        echo 'fetch OK'
+        git reset --hard origin/master
+        echo 'reset --hard OK'
+        git pull origin master
+        echo 'deployment success!'
+    fi
+"
--- resources/assets/build/config.js
+++ resources/assets/build/config.js
@@ -2,7 +2,9 @@ const path = require('path');
 const { argv } = require('yargs');
 const merge = require('webpack-merge');
 
-const userConfig = require('../config');
+const desire = require('./util/desire');
+
+const userConfig = merge(desire(`${__dirname}/../config`), desire(`${__dirname}/../config-local`));
 
 const isProduction = !!((argv.env && argv.env.production) || argv.p);
 const rootPath = (userConfig.paths && userConfig.paths.root)
@@ -10,6 +12,7 @@ const rootPath = (userConfig.paths && userConfig.paths.root)
   : process.cwd();
 
 const config = merge({
+  open: true,
   copy: 'images/**/*',
   proxyUrl: 'http://localhost:3000',
   cacheBusting: '[name]_[hash]',
@@ -22,7 +25,6 @@ const config = merge({
     sourceMaps: !isProduction,
     optimize: isProduction,
     cacheBusting: isProduction,
-    watcher: !!argv.watch,
   },
   watch: [],
 }, userConfig);
@@ -32,6 +34,10 @@ module.exports = merge(config, {
   publicPath: `${config.publicPath}/${path.basename(config.paths.dist)}/`,
   manifest: {},
 });
+
+if (process.env.NODE_ENV === undefined) {
+  process.env.NODE_ENV = isProduction ? 'production' : 'development';
+}
 
 /**
  * If your publicPath differs between environments, but you know it at compile time,
@@ -50,4 +56,4 @@ if (process.env.SAGE_DIST_PATH) {
  *   wp_localize_script('sage/main.js', 'SAGE_DIST_PATH', get_theme_file_uri('dist/'))
  */
 // Object.keys(module.exports.entry).forEach(id =>
-//   module.exports.entry[id].unshift(path.join(__dirname, 'public-path.js')));
+//   module.exports.entry[id].unshift(path.join(__dirname, 'helpers/public-path.js')));
