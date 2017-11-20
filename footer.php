e69af6b660bfdc56929d5f244e29d0aa3d7fdc32
千葉工 <chibataku0815@gmail.com>
2017-11-21 03:22:34 +0900

    base reset

--- footer.php
+++ footer.php
@@ -1,11 +1,8 @@
 <?php
 /**
  * The template for displaying the footer
- *
  * Contains the closing of the #content div and all content after.
- *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
  * @package info@fores-tone.co.jp
  */
 
@@ -14,17 +11,6 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'infofores-tone-co-jp' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'infofores-tone-co-jp' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'infofores-tone-co-jp' ), 'infofores-tone-co-jp', '<a href="http://underscores.me/">Underscores.me</a>' );
-			?>
-		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
